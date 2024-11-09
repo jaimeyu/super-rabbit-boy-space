a4ff464b29580e05102e7999688ce6db40d0bed3
Jaime Yu <jaime@jaimeyu.com>
2024-11-09 13:57:03 -0700

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,97 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "Ship flying":
+            case "anim1":return [img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . f f 1 9 9 1 f f . . . 
+. . . . f f 9 1 9 9 1 9 f f . . 
+. . . . f 9 1 f 1 1 f 1 9 f . . 
+. . . . f 9 1 1 1 1 1 1 9 f . . 
+5 . . . f f 1 f 1 1 f 1 f f . . 
+5 5 . f f f 9 1 f f 1 f f f f . 
+4 5 5 f d f f f f f f f 4 4 f f 
+4 4 5 f d f 4 4 4 4 4 4 4 4 f f 
+2 4 4 f d f 4 4 4 4 4 4 4 f f . 
+2 5 5 f f f f f f f f f f . . . 
+5 5 . . . . . . . . . . . . . . 
+5 . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . f f f f . . . . . 
+. . . . . f f 1 9 9 1 f f . . . 
+. . . . f f 9 1 9 9 1 9 f f . . 
+. . . . f 9 1 f 1 1 f 1 9 f . . 
+. . . . f 9 1 1 1 1 1 1 9 f . . 
+. . . . f f 1 f 1 1 f 1 f f . . 
+5 . . f f f 9 1 f f 1 f f f f . 
+2 5 . f d f f f f f f f 4 4 f f 
+2 2 5 f d f 4 4 4 4 4 4 4 4 f f 
+4 4 2 f d f 4 4 4 4 4 4 4 f f . 
+4 5 5 f f f f f f f f f f . . . 
+5 . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+            case "Robot":
+            case "anim2":return [img`
+. . . . . f f f f f f . . . . . 
+. . . . . f 2 6 6 2 f . . . . . 
+. . . . . f 2 6 6 2 f . . . . . 
+. . . . . f 6 5 5 6 f . . . . . 
+. . . . . f 5 6 6 5 f . . . . . 
+. f f f f f f f f f f f f f f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+f f f . f 1 6 6 6 6 6 f . f f f 
+f . f . f 1 1 1 1 1 1 f . f . f 
+f . f . f f f f f f f f . f . f 
+. . . . . . . 5 4 5 . . . . . . 
+. . . . . . 5 5 4 5 5 . . . . . 
+. . . . . . 5 4 4 4 5 . . . . . 
+`, img`
+. . . . . f f f f f f . . . . . 
+. . . . . f 6 6 6 2 f . . . . . 
+. . . . . f 2 6 6 2 f . . . . . 
+. . . . . f 6 5 5 6 f . . . . . 
+. . . . . f 5 6 6 5 f . . . . . 
+. f f f f f f f f f f f f f f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+. f . . f 1 6 6 6 6 6 f . . f . 
+f f . . f 1 6 6 6 6 6 f . . f f 
+f f . . f 1 1 1 1 1 1 f . . f f 
+f f . . f f f f f f f f . . f f 
+. . . . . . . 5 2 5 . . . . . . 
+. . . . . . 5 2 2 2 5 . . . . . 
+. . . . . . 2 2 4 2 2 . . . . . 
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
